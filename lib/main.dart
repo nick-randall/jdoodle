18ddefbaeda10c2d3415543e9111000d0b82c322
rabotnik1 <randall@posteo.de>
2023-04-14 14:08:49 +1000

    sending request successfully with wstoken and java script

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,46 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:stomp_dart_client/stomp.dart';
 import 'package:stomp_dart_client/stomp_config.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-const clientId = "f24fcb9aa1f0a602f3f645ff314c52a5";
-const clientSecret =
-    "ac75b0835426cec3ce1b2daf982d873c00d334fdc699ee8bcc6d56b909f9e0d2";
-const wsTokenUrl = "https://api.jdoodle.com/v1/auth-token";
-// const wsUrl = "wss://api.jdoodle.com/";
+const wsTokenAddress = "https://throbbing-snowflake-655.fly.dev/";
 const wsUrl = "wss://api.jdoodle.com/v1/stomp/792";
+
+const script = """ import java.util.Scanner;
+ import java.util.NoSuchElementException;
+
+public class MyClass {
+ public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+
+		try {
+		 System.out.println("Type a Line and enter....");
+		String txt = scanner.nextLine();
+		System.out.println("You have typed...");
+		System.out.println(txt);
+		} catch (NoSuchElementException e) {
+		    System.out.println("Type something in the Stdin box above....");
+		}
+
+	}
+  }""";
 
 void main() async {
   runApp(const ProviderScope(child: JdoodleApp()));
-  const headers = {
-    "Content-Type": "application/json; charset=UTF-8",
-  };
-  final data =
-      jsonEncode("{'clientId': '$clientId','clientSecret': '$clientSecret'}");
-  var request = Request('POST', Uri.parse(wsTokenUrl));
-  request.body = data;
-  request.headers.addAll(headers);
 
+  final wsTokenUrl = Uri.parse(wsTokenAddress);
+  final response = await http.post(wsTokenUrl);
+  final body = json.decode(response.body);
+  String token = body["token"];
+  final data = jsonEncode({
+    "script": script,
+    "language": "java",
+    "versionIndex": 4,
+  });
+  print(token);
   // StreamedResponse response = await request.send();
 
   // if (response.statusCode == 200) {
@@ -43,7 +60,8 @@ void main() async {
         print('connecting...');
       },
       onWebSocketError: (dynamic error) => print(error.toString()),
-      onConnect: (p0) => print(p0.headers),
+      onConnect: (frame) => print(
+          "body: ${frame.body} headers: ${frame.headers} command: ${frame.command}"),
       onStompError: (p1) => print(p1.body));
   StompClient client = StompClient(config: config);
   // StompClient client = StompClient(
@@ -66,24 +84,22 @@ void main() async {
   print(client.connected);
 
   void send() => client.send(
-          destination: '/app/execute-ws-api-token',
-          body: "hello world",
-          headers: {
-            "message_type": 'execute',
-            "token":
-                "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJKRE9PRExFIiwic3ViIjoiV1MtQVBJLVRPS0VOIiwiY2xpZW50LWlkIjoiZjI0ZmNiOWFhMWYwYTYwMmYzZjY0NWZmMzE0YzUyYTUiLCJpYXQiOjE2ODE0MjcyODUsImV4cCI6MTY4MTQyNzQ2NX0.pBS3Lg3WWkh3AWplmwjsVMhqQlPP8LvUIQ3q1wwndbs"
-          });
+      destination: '/app/execute-ws-api-token',
+      body: data,
+      headers: {"message_type": 'execute', "token": token});
 
   client.subscribe(
       destination: '/user/queue/execute-i',
       callback: (frame) {
         print("subscribe callback");
-        print("${frame.body} ${frame.headers}");
+        print(
+            "body: ${frame.body} headers: ${frame.headers} command: ${frame.command}");
       },
-      headers: {
-        "token":
-            "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJKRE9PRExFIiwic3ViIjoiV1MtQVBJLVRPS0VOIiwiY2xpZW50LWlkIjoiZjI0ZmNiOWFhMWYwYTYwMmYzZjY0NWZmMzE0YzUyYTUiLCJpYXQiOjE2ODE0MjcyODUsImV4cCI6MTY4MTQyNzQ2NX0.pBS3Lg3WWkh3AWplmwjsVMhqQlPP8LvUIQ3q1wwndbs" // "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJKRE9PRExFIiwic3ViIjoiV1MtQVBJLVRPS0VOIiwiY2xpZW50LWlkIjoiODk0ZGVhYmQ1ZGYwZDBmZGE0NmZjZDI5NGM0ZTZmNmEiLCJpYXQiOjE2Nzg4ODQ0MDYsImV4cCI6MTY3ODg4NDU4Nn0.Vp2rqQ9HQxtxq1XcuQtWSkmHA21GiJbZltNs49zqlAs"
-      });
+      headers: {"token": token});
+  await Future.delayed(const Duration(seconds: 2));
+
+  send();
+  await Future.delayed(const Duration(seconds: 2));
   send();
 
   // print(client.connected);
